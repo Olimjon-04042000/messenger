fdfd0f4c8f2879d6eb14dcd24355eece94c3ab42
Olimjon-04042000 <nishonovolimjon2000@gmail.com>
2024-04-03 22:36:34 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Typography } from "@mui/material"
+import Login from "./pages/Login"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>hello</h1>
+      <p>hello</p>
+      <Typography>hello</Typography>
+      <Login/>
+    </>
+  )
+}
+
+export default App
